a63daacbe341f326ff6232337ea11333328713e0
Thiago Yonekura <thiagoyonekura@gmail.com>
2023-11-18 23:35:52 -0300

    Updated data de nascimento

--- my-app/src/pages/telaCadastro.js
+++ my-app/src/pages/telaCadastro.js
@@ -15,33 +15,83 @@ export default function TelaCadastro(props){
     const {set} = useContext(MeuContexto)
     
     const handleLogin = async () => {
-        if(userSenha === confirma){
-          const dataNasc = new Date(nasc.slice(4,7), nasc.slice(2,3), nasc.slice(0,1), 0,0,0);
-          const data = {id:0, nome: userNome, cpf: userCpf, dataNascimento: dataNasc, email: userEmail, telefone: tel, senha: userSenha}
-          try {
-            const response = await fetch('https://projetointegrado2023-dev-tgsa.2.sg-1.fl0.io/api/usuario/UsuarioEPaciente', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(data),
-            });
-        
-        
-            if (response.status === 200 || response.status === 201) {
-              set(data)
-              Alert.alert('Cadastrado com sucesso!')
-              props.navigation.goBack()
-            } else {
-              Alert.alert('Erro', data.message || 'Erro ao cadastrar.');
-            }
-          } catch (error) {
-            Alert.alert('Erro', 'Não foi possível conectar ao servidor.');
+      if (userSenha === confirma) {
+          // Divide a data de nascimento em dia, mês e ano
+          const partesData = nasc.split('/');
+          if (partesData.length === 3) {
+              // Subtrai 1 do mês, pois os meses em JavaScript começam do 0
+              const ano = parseInt(partesData[2], 10);
+              const mes = parseInt(partesData[1], 10) - 1;
+              const dia = parseInt(partesData[0], 10);
+  
+              // Cria o objeto Date com ano, mês e dia
+              const dataNasc = new Date(ano, mes, dia);
+  
+              // Objeto com os dados do usuário
+              const data = {
+                  id: 0,
+                  nome: userNome,
+                  cpf: userCpf,
+                  dataNascimento: dataNasc.toISOString(), // Converte para o formato ISO para envio
+                  email: userEmail,
+                  telefone: tel,
+                  senha: userSenha
+              };
+  
+              try {
+                  // Faz a requisição POST com os dados do usuário
+                  const response = await fetch('https://projetointegrado2023-dev-tgsa.2.sg-1.fl0.io/api/usuario/UsuarioEPaciente', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify(data),
+                  });
+  
+                  if (response.status === 200 || response.status === 201) {
+                      set(data);
+                      Alert.alert('Cadastrado com sucesso!');
+                      props.navigation.goBack();
+                  } else {
+                      const responseData = await response.json(); // Obtem a resposta JSON
+                      Alert.alert('Erro', responseData.message || 'Erro ao cadastrar.');
+                  }
+              } catch (error) {
+                  Alert.alert('Erro', 'Não foi possível conectar ao servidor.');
+              }
+          } else {
+              Alert.alert('Erro', 'Formato de data de nascimento inválido.');
           }
-        }else{
+      } else {
           Alert.alert('Os campos de senha devem ser iguais');
+      }
+  };
+
+      const formatarData = (data) => {
+        // Remove qualquer caractere que não seja número
+        const numeros = data.replace(/[^0-9]/g, '');
+    
+        // Dividir a string em partes para dia, mês e ano
+        let dia = numeros.slice(0, 2);
+        let mes = numeros.slice(2, 4);
+        let ano = numeros.slice(4, 8);
+    
+        // Montar a data formatada
+        let dataFormatada = dia;
+        if (dia.length === 2 && numeros.length > 2) {
+            dataFormatada += '/' + mes;
+            if (mes.length === 2 && numeros.length > 4) {
+                dataFormatada += '/' + ano;
+            }
         }
-      };
+    
+        return dataFormatada;
+    }
+    
+    const handleNascChange = (data) => {
+        const dataFormatada = formatarData(data);
+        setNasc(dataFormatada);
+    }
       
     const BotaoCadastro = styled.TouchableOpacity`
     background-color:#000;
@@ -100,13 +150,13 @@ export default function TelaCadastro(props){
             />
             <TextInput 
             keyboardType="numeric" 
-            maxLength={8} 
-            placeholder="Data de Nascimento" 
+            maxLength={10} 
+            placeholder="Data de Nascimento (DD/MM/YYYY)" 
             placeholderTextColor={'#555'}
             style={Estilo.loginTextInputs}
             value={nasc}
-            onChangeText={ (nasc) => setNasc(nasc)}
-            />
+            onChangeText={handleNascChange}
+        />
             <TextInput 
             caretHidden={false} 
             maxLength={20} 
